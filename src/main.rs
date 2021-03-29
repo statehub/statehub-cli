9265c465e30152268d0f2b72489d493c4dfbc1e6
Cyril Plisko <cyril.plisko@replix.io>
2021-03-29 11:28:07 +0000

    Add structopt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+//
+// Copyright (c) 2021 RepliXio Ltd. All rights reserved.
+// Use is subject to license terms.
+//
+
+#![cfg_attr(feature = "pedantic", warn(clippy::pedantic))]
+#![warn(clippy::use_self)]
+#![warn(deprecated_in_future)]
+#![warn(future_incompatible)]
+#![warn(unreachable_pub)]
+#![warn(missing_debug_implementations)]
+#![warn(rust_2018_compatibility)]
+#![warn(rust_2018_idioms)]
+#![warn(unused)]
+#![deny(warnings)]
+
 fn main() {
-    println!("Hello, world!");
+    println!("statehub CLI");
 }
