4bf8c58787e7409a3731b0776202524a3a440da2
Cyril Plisko <cyril.plisko@replix.io>
2021-04-04 12:15:49 +0000

    High level commands

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,98 @@
 #![warn(unused)]
 #![deny(warnings)]
 
-fn main() {
-    println!("statehub CLI");
+use structopt::StructOpt;
+
+const ABOUT: &str = "statehub CLI tool";
+
+fn main() -> anyhow::Result<()> {
+    dotenv::dotenv().ok();
+    pretty_env_logger::init_custom_env("STATEHUB_LOG");
+    StateHub::from_args().execute()
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(about = ABOUT)]
+struct StateHub {
+    #[structopt(short, long)]
+    verbose: bool,
+    #[structopt(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, StructOpt)]
+enum Command {
+    #[structopt(about = "Create new state")]
+    CreateState,
+    #[structopt(about = "Register new cluster")]
+    RegisterCluster,
+    #[structopt(about = "Unregister existing cluster")]
+    UnregisterCluster,
+    #[structopt(about = "Manually create new volume")]
+    CreateVolume,
+    #[structopt(about = "Manually delete existing volume")]
+    DeleteVolume,
+    #[structopt(about = "Manually make state available in a specified location")]
+    AddLocation,
+    #[structopt(about = "Manually make state unavailable in a specified location")]
+    RemoveLocation,
+    #[structopt(about = "Set state availability grade")]
+    SetAvailability,
+    #[structopt(about = "Set specified cluster as owner for a given state")]
+    SetOwner,
+}
+
+impl StateHub {
+    fn execute(self) -> anyhow::Result<()> {
+        println!("{}", self.verbose);
+        match self.command {
+            Command::CreateState => Command::create_state(),
+            Command::RegisterCluster => Command::register_cluster(),
+            Command::UnregisterCluster => Command::unregister_cluster(),
+            Command::CreateVolume => Command::create_volume(),
+            Command::DeleteVolume => Command::delete_volume(),
+            Command::AddLocation => Command::add_location(),
+            Command::RemoveLocation => Command::remove_location(),
+            Command::SetAvailability => Command::set_availability(),
+            Command::SetOwner => Command::set_owner(),
+        }
+    }
+}
+
+impl Command {
+    fn create_state() -> anyhow::Result<()> {
+        Ok(())
+    }
+
+    fn register_cluster() -> anyhow::Result<()> {
+        Ok(())
+    }
+
+    fn unregister_cluster() -> anyhow::Result<()> {
+        Ok(())
+    }
+
+    fn create_volume() -> anyhow::Result<()> {
+        Ok(())
+    }
+
+    fn delete_volume() -> anyhow::Result<()> {
+        Ok(())
+    }
+
+    fn add_location() -> anyhow::Result<()> {
+        Ok(())
+    }
+
+    fn remove_location() -> anyhow::Result<()> {
+        Ok(())
+    }
+
+    fn set_availability() -> anyhow::Result<()> {
+        Ok(())
+    }
+
+    fn set_owner() -> anyhow::Result<()> {
+        Ok(())
+    }
 }
